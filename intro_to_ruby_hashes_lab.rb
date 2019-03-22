ff010ef247fbb309c193af68408c1b432e33fbc9
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-22 17:48:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,9 +38,16 @@ def monopoly_with_third_tier
 	      :reading_rainbow => {
 	        "mortgage_value" => "$400" 
 	      },
-	      :pennsylvania_railroad => {},
-	      :b_and_o_railroad => {},
-	      :shortline => {},
+	      :pennsylvania_railroad => {
+	        "mortgage_value" => "$200"
+	      },
+	      :b_and_o_railroad => {
+	        "mortgage_value" => "$400"
+	        
+	      },
+	      :shortline => {
+	        "mortgage_value" => "$800"
+	      },
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
